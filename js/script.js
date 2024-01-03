b80cb442c21aa0572f57d8df3d9c56bb2c684cc3
Farhan <farhanyuzaa45@gmail.com>
2024-01-03 15:14:10 +0700

    First Commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Toggle class active
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+// Ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+}
+
+
+// Klik di luar side bar untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
